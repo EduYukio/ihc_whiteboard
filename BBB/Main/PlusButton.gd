5bab34f5c6c4da197183bd69dbf01a176c0b226c
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-07-20 22:58:25 -0300

    Now, the "plus" button popup appears and disappears correctly

--- BBB/Main/PlusButton.gd
+++ BBB/Main/PlusButton.gd
@@ -1,21 +1,15 @@
 extends Button
 
 const PlusPopupScene = preload("res://BBB//PlusMenu//PlusPopup.tscn")
+var PlusPopupInstance
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_PlusButton_pressed():
-	var PlusPopupInstance = PlusPopupScene.instance()
-	self.add_child(PlusPopupInstance)
-	PlusPopupInstance.position = PlusPopupInstance.position + Vector2(0,-82)
+	if not has_node("PlusPopup"): 
+		PlusPopupInstance = PlusPopupScene.instance()
+		self.add_child(PlusPopupInstance)
+		PlusPopupInstance.position = PlusPopupInstance.position + Vector2(0,-82)
+	else:
+		PlusPopupInstance.queue_free()
